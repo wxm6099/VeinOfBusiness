a790b2d9e93660796a8c2d07933577f7c0f6425c
ShenJiaHao <13238041502@163.com>
2016-11-24 01:55:09 +0800

    完成积分的兑换及历史展示功能, 尚未完善

---
+++ VeinOfBusiness/Sections/MoneyReward/Controller/IntegralExchangeHistoryView.h
@@ -0,0 +1,13 @@
+//
+//  IntegralExchangeHistoryView.h
+//  VeinOfBusiness
+//
+//  Created by 太阳 on 2016/11/24.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntegralExchangeHistoryView : UIViewController
+
+@end
